28daa2459eed177494a8348b9a12869b45d456fd
Akshay Gupta <kitallis@gmail.com>
2023-03-22 15:13:39 +0530

    Add a splash of tramline green

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,15 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-      "./content/*.md",
-      "./templates/*.html",
-    ],
+    "./content/*.md",
+    "./templates/*.html",
+  ],
   theme: {
     extend: {
+      colors: {
+        'tram-green': '#1a9c1d',
+        'tram-green-light': '#f6f5e9',
+      },
       fontFamily: {
         'cardo': ['Cardo', ...defaultTheme.fontFamily.serif],
         'inter': ['Inter var', 'Inter', ...defaultTheme.fontFamily.sans],
